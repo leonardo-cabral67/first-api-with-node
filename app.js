9e994acce3317eb2e58c39a2f9e37e6cad727414
Leonardo Cabral <leonardolccabral@gmail.com>
2022-10-20 19:20:37 -0300

    feat: installed express, nodemon and created my first post method that add products to products array (in memory)

---
+++ app.js
@@ -0,0 +1,25 @@
+const { request } = require("express");
+const express = require("express");
+const { randomUUID } = require("crypto");
+
+const app = express();
+
+app.use(express.json());
+
+const products = [];
+
+app.post("/products", (req, res) => {
+  const { name, price } = req.body;
+
+  const product = {
+    name,
+    price,
+    id: randomUUID(),
+  };
+
+  products.push(product);
+
+  return res.json(product);
+});
+
+app.listen(4002, () => console.log("Servidor está rodando na porta 4002"));
